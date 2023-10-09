3b4b0673fd75829bc92489fce00ecc4cee69cdef
takuya.0810@hotmail.co.jp <takuya-tokumoto>
2023-10-09 04:38:35 +0000

    おにぎり

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ docker run --rm -it \
     -e LOCAL_UID=$(id -u $USER) \
     -e LOCAL_GID=$(id -g $USER) \
     -e PYTHONPATH=/home/developer/transformers:/home/developer/transformers/experiments/minigrid/dependencies/gym-minigrid:/home/developer/transformers/experiments/minigrid/dependencies/torch-ac \
-    -v /$(pwd -W):/home/jovyan/env_alaysis\
+    -v /$(pwd -W)/..:/home/jovyan/env_alaysis\
     -p 8888:8888 \
     --name $(id -u -n)-env_analysis \
     ${IMAGE_NAME}
